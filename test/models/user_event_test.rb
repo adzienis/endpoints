be9980a9d85496bf71dbfb0711b1a49be87f30c0
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-01 15:17:03 -0400

    added basic tests for user events

--- test/models/user_event_test.rb
+++ test/models/user_event_test.rb
@@ -4,4 +4,40 @@ class UserEventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "valid user_event" do
+    UserEvent.create({
+                       data: {
+                         name: "key",
+                         event_type: "click"
+                       }
+                     })
+    assert(UserEvent.all.count == 1)
+  end
+  test "missing key name" do
+
+    UserEvent.create({
+                       data: {
+                         event_type: "click"
+                       }
+                     })
+    assert(UserEvent.all.count == 0)
+  end
+  test "missing key event_type" do
+
+    UserEvent.create({
+                       data: {
+                         name: "key"
+                       }
+                     })
+
+    assert(UserEvent.all.count == 0)
+  end
+
+  test "user event is nil" do
+
+    UserEvent.create({})
+
+    assert(UserEvent.all.count == 0)
+  end
 end
