ecc5a5c8f60b18fc2dd393b427fb961b187cb127
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-01 15:35:19 -0400

    added basic event functionality

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,2 +1,17 @@
 class UserEvent < ApplicationRecord
+
+  enum event_type: {click: "click", hover: "hover"}
+
+  validates :data, presence: true
+  validate :valid_blob, on: :create
+
+  def valid_blob
+
+    errors.add(:data, "missing event_type") unless data&.key?("event_type")
+    errors.add(:data, "missing name") unless data&.key?("name")
+
+    errors.add(:data, "invalid event_type") unless UserEvent.event_types[data&.fetch("event_type", nil)]
+  end
+
+
 end
