203f4b0de107b4ce358ea573f318525620b244e4
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-01 16:36:47 -0400

    todays stats test

--- test/models/user_event_test.rb
+++ test/models/user_event_test.rb
@@ -7,11 +7,11 @@ class UserEventTest < ActiveSupport::TestCase
 
   test "valid user_event" do
     event = UserEvent.create({
-                       data: {
-                         name: "key",
-                         event_type: "click"
-                       }
-                     })
+                               data: {
+                                 name: "key",
+                                 event_type: "click"
+                               }
+                             })
     assert(UserEvent.all.count == 1)
   end
 
@@ -50,5 +50,46 @@ class UserEventTest < ActiveSupport::TestCase
     UserEvent.create({})
 
     assert(UserEvent.all.count == 0)
+  end
+
+  test "todays_stats" do
+    UserEvent.create((1..5).map do
+      {
+        data: {
+          name: "key",
+          event_type: "click"
+        }
+      }
+    end + (1..5).map do
+      {
+        data: {
+          name: "key",
+          event_type: "hover"
+        }
+      }
+    end)
+
+    assert UserEvent.todays_stats.to_json == [{ "click": 5 }, { "hover": 5 }].to_json
+  end
+
+  test "todays_stats only includes today" do
+    UserEvent.create((1..5).map do
+      {
+        data: {
+          name: "key",
+          event_type: "click"
+        },
+        created_at: Date.yesterday
+      }
+    end + (1..5).map do
+      {
+        data: {
+          name: "key",
+          event_type: "hover"
+        }
+      }
+    end)
+
+    assert UserEvent.todays_stats.to_json == [{ "hover": 5 }].to_json
   end
 end
