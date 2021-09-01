9da3a2f1895bfff92ee6321446ab464682b3e09f
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-01 15:54:03 -0400

    added basic tests for events controller

--- test/controllers/user_events_controller_test.rb
+++ test/controllers/user_events_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserEventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "no body" do
+    post create_user_event_path
+    assert_response 422
+  end
+
+  test "valid event" do
+    post create_user_event_path, params: {
+      event: {
+        name: "foo",
+        event_type: "click"
+      }
+    }, as: :json
+    assert_response 201
+  end
 end
