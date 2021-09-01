14339cf1efc202399210b087ff6a3577be25c0e2
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-01 16:37:02 -0400

    added todays stats

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -13,5 +13,18 @@ class UserEvent < ApplicationRecord
     errors.add(:data, "invalid event_type") unless UserEvent.event_types[data&.fetch("event_type", nil)]
   end
 
+  def self.todays_stats
+    todays_events = UserEvent.where(created_at: Date.today.all_day)
+    grouped_events = todays_events.group("data ->> 'event_type'")
+                                  .select("data ->> 'event_type' as event_type, count(id)")
+
+    big_hash = ActiveRecord::Base.connection
+                                 .exec_query(grouped_events.to_sql)
+                                 .rows.to_h
+
+    big_hash.entries.map{|key,value| { key => value} }
+
+  end
+
 
 end
